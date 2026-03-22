463daad29ed335ab116cab60e2d41e89a48a5ad5
fosforino <fulviofosforino@gmail.com>
2026-03-22 07:27:05 +0100

    Allineamento finale immagini e percorsi

--- pages/Premio.py
+++ pages/Premio.py
@@ -1,23 +1,21 @@
-# pages/Premio.py
 import streamlit as st
 
 def show():
-    # Titolo della pagina
-    st.markdown("<h1 style='text-align: center; color: #3e2723; font-family: \"Playfair Display\";'>Il Tuo Riconoscimento Poetetico</h1>", unsafe_allow_html=True)
+    # Titolo della pagina - Coerente con la SEO "Poeticamente"
+    st.markdown("<h1 style='text-align: center; color: #3e2723; font-family: \"Playfair Display\";'>Il Tuo Riconoscimento</h1>", unsafe_allow_html=True)
     st.markdown("---")
 
-    # --- IL MOTORE DEL MEDAGLIONE FRONTE-RETRO ---
-    # Questa struttura HTML crea il "sandwich 3D" controllato dal CSS
+    # --- MOTORE 3D: IL SANDWICH DELLE TUE GRAFICHE ---
     st.markdown("""
         <div class="medaglione-3d-container">
             <div class="card-3d">
                 
                 <div class="faccia fronte">
-                    <img src="https://raw.githubusercontent.com/fosforino/Poeticamente/main/Poeticamente.png">
+                    <img src="https://raw.githubusercontent.com/fosforino/Poeticamente/main/Poeticamente_Fronte.png">
                 </div>
                 
-                <div class="faccia retro">
-                    <div class="testo-retro">by fosforino</div>
+                <div class="faccia retro-immagine">
+                    <img src="https://raw.githubusercontent.com/fosforino/Poeticamente/main/Poeticamente_Retro.png">
                 </div>
                 
             </div>
@@ -31,11 +29,13 @@ def show():
                 <i>"A chi ha saputo trasformare il silenzio in inchiostro,<br>
                 e il pensiero in un'opera senza tempo."</i>
             </p>
-            <p style="font-size: 1.1rem; color: #795548; margin-top: 20px;">
-                Con stima e ammirazione,<br>
-                la Comunità di Poeticamente.
+            <p style="font-size: 1.1rem; color: #795548; margin-top: 25px;">
+                Un riconoscimento da parte di chi crede nel valore<br>
+                <b>dell'Alchimia tra il Pensiero e il Verso.</b>
             </p>
-            <div class="firma-fosforino">by fosforino</div>
+            <div class="firma-fosforino">
+                fosforino
+            </div>
         </div>
     """, unsafe_allow_html=True)
 
